dbdd74f8f7c80779359f52faac3ee62c04e1cdbc
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-02 17:10:39 +0200

    add endpoint api for favourites

--- app/Models/Api/ProjectController.php
+++ app/Models/Api/ProjectController.php
@@ -11,10 +11,19 @@ class ProjectController extends Model
     use HasFactory;
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->orderByDesc('favourites')->paginate(4);
+        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(4);
         return response()->json([
             'success' => true,
             'projects' => $projects
+        ]);
+    }
+
+    public function favourites()
+    {
+        $favourites = Project::with('type', 'technologies')->where('favourites', true)->orderByDesc('favourites')->paginate(100);
+        return response()->json([
+            'success' => true,
+            'favourites' => $favourites
         ]);
     }
 }
